829984abedf1e339d98eb0cb2eccefeab9787f0a
Viper <viper666@inbox.ru>
2017-03-09 16:43:22 +0300

    Version for Azure upload

--- TodoWebApi.Core/Concrete/TodoDBContext.cs
+++ TodoWebApi.Core/Concrete/TodoDBContext.cs
@@ -10,7 +10,8 @@ namespace TodoWebApi.Core.Concrete
 {
     public class TodoDBContext : DbContext
     {
-        public TodoDBContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\azure\TodoWebApi\TodoWebApi\App_Data\TodoDB.mdf;Integrated Security=True")
+        //public TodoDBContext() : base(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\azure\TodoWebApi\TodoWebApi\App_Data\TodoDB.mdf;Integrated Security=True")
+        public TodoDBContext() : base(@"Data Source=adtodo.database.windows.net;Initial Catalog=TodoDB;Integrated Security=False;User ID=adodalev;Password=Demigod666;Connect Timeout=60;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False")
         {   
             Database.SetInitializer(new TodoDBInitializer());
         }
